728a71ff251cbe74fed76993d7b0127228d2bd23
MichalWojcieszyk <mwojcieszyk@gmail.com>
2016-04-19 20:01:46 +0200

    change in file

--- kalkulator.py
+++ kalkulator.py
@@ -1,13 +1,13 @@
 import math
 
-def heron (a ,b , c ):
+def heron (a ,b , c):
     """ Oblicza pole trojkata wg wzoru Herona
     a ,b ,c dlugosci bokow trojkata
     >> heron (3 ,4 ,5)
     6.0
     """
-    s = ( a + b + c )/2
-    return math . sqrt ( s * (s -1) * (s - b ) * (s - c ))
+    x = ( a + b + c )/2
+    return math . sqrt ( x * (x -1) * (x - b ) * (x - c ))
 
 if __name__ == "__main__":
     print (heron.__doc__)
