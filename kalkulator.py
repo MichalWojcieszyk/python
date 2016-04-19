8f61808b809636ab4d3bfc142ec2e72957052420
MichalWojcieszyk <mwojcieszyk@gmail.com>
2016-04-19 20:04:31 +0200

    poprawka in python branch

--- kalkulator.py
+++ kalkulator.py
@@ -1,6 +1,6 @@
 import math
 
-def heron (a ,b , c):
+def oblicz (a ,b , c):
     """ Oblicza pole trojkata wg wzoru Herona
     a ,b ,c dlugosci bokow trojkata
     >> heron (3 ,4 ,5)
@@ -10,5 +10,5 @@ def heron (a ,b , c):
     return math . sqrt ( x * (x -1) * (x - b ) * (x - c ))
 
 if __name__ == "__main__":
-    print (heron.__doc__)
-    print(heron(3, 4, 5))
+    print (oblicz.__doc__)
+    print(oblicz(3, 4, 5))
